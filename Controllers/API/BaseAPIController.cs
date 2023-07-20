bd73d25dcde34742d7f927cbcda4d26d5f780dd1
Marcos <marcosbustamantemateo@gmail.com>
2023-07-21 00:21:34 +0200

    configuro autorización JWT de API solo en modo PROD

--- Controllers/API/BaseAPIController.cs
+++ Controllers/API/BaseAPIController.cs
@@ -1,61 +1,63 @@
 ﻿using GenericControllerLib.BusinessLogic;
 using GenericControllerLib.Dto;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc; 
+using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Linq.Dynamic.Core;
 
 namespace GenericControllerLib.Controllers.API
 {
-    /// <summary>
-    ///     Controlador base que proporciona métodos CRUD para cualquier tipo de objeto
-    /// </summary>
-    /// <typeparam name="T">Entidad tipo T</typeparam>
-    //[Authorize]
-    [Route("api/[controller]")]
-    [ApiController]
-    [ProducesResponseType(typeof(BaseDto), 200)]
-    [ProducesResponseType(typeof(BaseDto), 400)]
-    [ProducesResponseType(typeof(BaseDto), 401)]
-    [ProducesResponseType(typeof(BaseDto), 500)]
-    public class BaseAPIController<T> : ControllerBase where T : class
-    {
-        private readonly BusinessLogic<T> _businessLogic;
+	/// <summary>
+	///     Controlador base que proporciona métodos CRUD para cualquier tipo de objeto
+	/// </summary>
+	/// <typeparam name="T">Entidad tipo T</typeparam>
+#if PROD
+	[Authorize]
+#endif
+	[Route("api/[controller]")]
+	[ApiController]
+	[ProducesResponseType(typeof(BaseDto), 200)]
+	[ProducesResponseType(typeof(BaseDto), 400)]
+	[ProducesResponseType(typeof(BaseDto), 401)]
+	[ProducesResponseType(typeof(BaseDto), 500)]
+	public class BaseAPIController<T> : ControllerBase where T : class
+	{
+		private readonly BusinessLogic<T> _businessLogic;
 
-        public BaseAPIController(BusinessLogic<T> businessLogic)
-        {
-            _businessLogic = businessLogic;
-        }
+		public BaseAPIController(BusinessLogic<T> businessLogic)
+		{
+			_businessLogic = businessLogic;
+		}
 
-        /// <summary>
-        ///     Crea una nueva entidad
-        /// </summary>
-        /// <param name="entity">Objeto a crear</param>
-        /// <returns>Resultado de la operación</returns>
-        //[Authorize(Roles = "Superadmin, Admin")]
-        [HttpPost("Create_Generic")]
-        public IActionResult Create(T entity)
-        {
-            try
-            {
-                if (entity != null) 
-                {
-                    var result = _businessLogic.Create(entity);
-                    if (result.rowsAffected == 1)
-                        return Ok(new BaseDto($"¡Objeto creado con éxito!", result));
-                    else    
-                        return StatusCode(500, new BaseDto($"¡No se pudo crear el objeto del tipo {typeof(T)}!", result));
-                }
-                else
-                {
-                    return BadRequest(new BaseDto("¡Error: El objeto enviado es nulo o no coincide con la definición!", null));
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new BaseDto($"¡No se pudo crear el objeto del tipo {typeof(T)}!", ex.InnerException != null ? ex.InnerException.Message : ex.Message));
-            }
-        }
+		/// <summary>
+		///     Crea una nueva entidad
+		/// </summary>
+		/// <param name="entity">Objeto a crear</param>
+		/// <returns>Resultado de la operación</returns>
+		//[Authorize(Roles = "Superadmin, Admin")]
+		[HttpPost("Create_Generic")]
+		public IActionResult Create(T entity)
+		{
+			try
+			{
+				if (entity != null)
+				{
+					var result = _businessLogic.Create(entity);
+					if (result.rowsAffected == 1)
+						return Ok(new BaseDto($"¡Objeto creado con éxito!", result));
+					else
+						return StatusCode(500, new BaseDto($"¡No se pudo crear el objeto del tipo {typeof(T)}!", result));
+				}
+				else
+				{
+					return BadRequest(new BaseDto("¡Error: El objeto enviado es nulo o no coincide con la definición!", null));
+				}
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, new BaseDto($"¡No se pudo crear el objeto del tipo {typeof(T)}!", ex.InnerException != null ? ex.InnerException.Message : ex.Message));
+			}
+		}
 
 		/// <summary>
 		///     Devuelve un listado de entidad dada
@@ -68,109 +70,109 @@ namespace GenericControllerLib.Controllers.API
 		/// <param name="excludeActived">Indica si se excluyen los elementos dados de alta</param>
 		/// <returns>Listado de entidad dada</returns>
 		[HttpGet("Read_Generic")]
-        public ActionResult Read(int page = 1, int pageSize = 10, string includes = "", string filter = "", 
-                                    [Required] bool includeDeleted = true, [Required] bool excludeActived = false)
-        {
-            try
-            {
-                if (page >= 1 && pageSize >= 1)
-                {
-                    var result = _businessLogic.Read(page, pageSize, filter, includeDeleted, excludeActived, includes);
+		public ActionResult Read(int page = 1, int pageSize = 10, string includes = "", string filter = "",
+									[Required] bool includeDeleted = true, [Required] bool excludeActived = false)
+		{
+			try
+			{
+				if (page >= 1 && pageSize >= 1)
+				{
+					var result = _businessLogic.Read(page, pageSize, filter, includeDeleted, excludeActived, includes);
 					PagedResult<T>? pagedResultAux = result.data as PagedResult<T>;
 
-                    return Ok(new BaseDto($"Objeto tipo: {typeof(T)}" +
-                        $"Total elementos: {pagedResultAux.Queryable.Count()}. " +
-                        (page != -1
-                            ? ($"Elementos por página: {pagedResultAux.PageSize}. "
-                                + $"Mostrando página {pagedResultAux.CurrentPage} de {pagedResultAux.PageCount}. ")
-                            : $"Mostrando todos los elementos (sin paginación).")
-                        , result));
-                }
-                else
-                {
-                    return BadRequest(new BaseDto("¡Error: El parámetro page o pageSize no puede ser inferior a 1!", new { page, pageSize }));
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new BaseDto("¡Error: No se pudo listar!", ex.Message));
-            }
-        }
+					return Ok(new BaseDto($"Objeto tipo: {typeof(T)}" +
+						$"Total elementos: {pagedResultAux.Queryable.Count()}. " +
+						(page != -1
+							? ($"Elementos por página: {pagedResultAux.PageSize}. "
+								+ $"Mostrando página {pagedResultAux.CurrentPage} de {pagedResultAux.PageCount}. ")
+							: $"Mostrando todos los elementos (sin paginación).")
+						, result));
+				}
+				else
+				{
+					return BadRequest(new BaseDto("¡Error: El parámetro page o pageSize no puede ser inferior a 1!", new { page, pageSize }));
+				}
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, new BaseDto("¡Error: No se pudo listar!", ex.Message));
+			}
+		}
 
-        /// <summary>
-        ///     Actualiza una entidad
-        /// </summary>
-        /// <param name="entity">Objeto a actualizar</param>
-        /// <returns>Resultado de la operación</returns>
-        //[Authorize(Roles = "Superadmin, Admin")]
-        [HttpPut("Update_Generic")]
-        public IActionResult Update(T entity)
-        {
-            try
-            {
-                if (entity != null)
-                {
+		/// <summary>
+		///     Actualiza una entidad
+		/// </summary>
+		/// <param name="entity">Objeto a actualizar</param>
+		/// <returns>Resultado de la operación</returns>
+		//[Authorize(Roles = "Superadmin, Admin")]
+		[HttpPut("Update_Generic")]
+		public IActionResult Update(T entity)
+		{
+			try
+			{
+				if (entity != null)
+				{
 
-                    var result = _businessLogic.Update(entity);
-                    if (result.rowsAffected == 1)
-                        return Ok(new BaseDto($"¡Objeto del tipo {typeof(T)} actualizado con éxito!", result));
-                    else
-                        return StatusCode(500, new BaseDto($"¡No se pudo actualizar el objeto del tipo {typeof(T)}!", result));
-                }
-                else
-                {
-                    return BadRequest(new BaseDto("¡Error: El objeto enviado es nulo o no coincide con la definición!", null));
-                }
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new BaseDto($"¡No se pudo actualizar el objeto del tipo {typeof(T)}!", ex.Message));
-            }
-        }
+					var result = _businessLogic.Update(entity);
+					if (result.rowsAffected == 1)
+						return Ok(new BaseDto($"¡Objeto del tipo {typeof(T)} actualizado con éxito!", result));
+					else
+						return StatusCode(500, new BaseDto($"¡No se pudo actualizar el objeto del tipo {typeof(T)}!", result));
+				}
+				else
+				{
+					return BadRequest(new BaseDto("¡Error: El objeto enviado es nulo o no coincide con la definición!", null));
+				}
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, new BaseDto($"¡No se pudo actualizar el objeto del tipo {typeof(T)}!", ex.Message));
+			}
+		}
 
-        /// <summary>
-        ///     Da de baja una entidad dado su id
-        /// </summary>
-        /// <param name="id">Id de la entidad</param>
-        /// <param name="saveData">Indica si en lugar de borrar el objeto solo da de baja el objeto</param>
-        /// <returns>Resultado de la operación</returns>
-        //[Authorize(Roles = "Superadmin, Admin")]
-        [HttpDelete("Delete_Generic")]
-        public IActionResult Delete(int id, bool saveData = true)
-        {
-            try
-            {
-                if (id > 0)
-                {
-                    var result = _businessLogic.Delete(id, saveData);
-                    if (result.rowsAffected == 1)
-                    {
-                        if (saveData)
-                            return Ok(new BaseDto($"¡Objeto del tipo {typeof(T)} con id {id} dado de baja con éxito!", result));
-                        else 
-                            return Ok(new BaseDto($"¡Objeto del tipo {typeof(T)} con id {id} eliminado con éxito!", result));
-                    } 
-                    else
-                    {
+		/// <summary>
+		///     Da de baja una entidad dado su id
+		/// </summary>
+		/// <param name="id">Id de la entidad</param>
+		/// <param name="saveData">Indica si en lugar de borrar el objeto solo da de baja el objeto</param>
+		/// <returns>Resultado de la operación</returns>
+		//[Authorize(Roles = "Superadmin, Admin")]
+		[HttpDelete("Delete_Generic")]
+		public IActionResult Delete(int id, bool saveData = true)
+		{
+			try
+			{
+				if (id > 0)
+				{
+					var result = _businessLogic.Delete(id, saveData);
+					if (result.rowsAffected == 1)
+					{
+						if (saveData)
+							return Ok(new BaseDto($"¡Objeto del tipo {typeof(T)} con id {id} dado de baja con éxito!", result));
+						else
+							return Ok(new BaseDto($"¡Objeto del tipo {typeof(T)} con id {id} eliminado con éxito!", result));
+					}
+					else
+					{
 						if (saveData)
 							return StatusCode(500, new BaseDto($"¡No se pudo dar de baja el objeto del tipo {typeof(T)} con id {id}!", result));
 						else
 							return StatusCode(500, new BaseDto($"¡No se pudo eliminar el objeto del tipo {typeof(T)} con id {id}!", result));
 					}
-                }
-                else
-                {
-                    return BadRequest(new BaseDto("¡Error: El parámetro id no puede ser inferior a 1!", null));
-                }
-            }
-            catch (Exception ex)
-            {
-                if (saveData)
-                    return StatusCode(500, new BaseDto($"¡No se pudo dar de baja el objeto del tipo {typeof(T)} con id {id}!", ex.InnerException != null ? ex.InnerException.Message : ex.Message));
-                else
-                    return StatusCode(500, new BaseDto($"¡No se pudo eliminar el objeto del tipo {typeof(T)} con id {id}!", ex.InnerException != null ? ex.InnerException.Message : ex.Message));
-            }
-        }
+				}
+				else
+				{
+					return BadRequest(new BaseDto("¡Error: El parámetro id no puede ser inferior a 1!", null));
+				}
+			}
+			catch (Exception ex)
+			{
+				if (saveData)
+					return StatusCode(500, new BaseDto($"¡No se pudo dar de baja el objeto del tipo {typeof(T)} con id {id}!", ex.InnerException != null ? ex.InnerException.Message : ex.Message));
+				else
+					return StatusCode(500, new BaseDto($"¡No se pudo eliminar el objeto del tipo {typeof(T)} con id {id}!", ex.InnerException != null ? ex.InnerException.Message : ex.Message));
+			}
+		}
 
 		/// <summary>
 		///     Da de alta una entidad dado su id
@@ -207,22 +209,22 @@ namespace GenericControllerLib.Controllers.API
 		/// <param name="value">Valor de la propiedad a filtrar</param>
 		/// <returns>Entidad dada</returns>
 		[HttpGet("ReadByKey_Generic")]
-        public ActionResult ReadByKey(string key, string value)
-        {
-            try
-            {
-                if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(key))
-                {
-                    var result = _businessLogic.GetByKey(key, value);
-                    return Ok(new BaseDto($"¡Encontrado el objeto del tipo: {typeof(T)} con clave-valor: {key}-{value}!", result));
-                }
-                else
-                    return BadRequest(new BaseDto("¡Error: El parámetro key y/o value no pueden estar vacios!", new { key, value }));
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(500, new BaseDto($"¡Error: No se pudo encontrar la entidad donde el parámetro '{key}' es igual a '{value}'!", ex.Message));
-            }
-        }
-    }
+		public ActionResult ReadByKey(string key, string value)
+		{
+			try
+			{
+				if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(key))
+				{
+					var result = _businessLogic.GetByKey(key, value);
+					return Ok(new BaseDto($"¡Encontrado el objeto del tipo: {typeof(T)} con clave-valor: {key}-{value}!", result));
+				}
+				else
+					return BadRequest(new BaseDto("¡Error: El parámetro key y/o value no pueden estar vacios!", new { key, value }));
+			}
+			catch (Exception ex)
+			{
+				return StatusCode(500, new BaseDto($"¡Error: No se pudo encontrar la entidad donde el parámetro '{key}' es igual a '{value}'!", ex.Message));
+			}
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -15,138 +15,147 @@ var builder = WebApplication.CreateBuilder(args);
 // Contexts
 #if DEV
 builder.Services
-    .AddDbContext<EntitiesDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DEV_EntitiesConnection")));
+	.AddDbContext<EntitiesDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DEV_EntitiesConnection")));
 #endif
 #if PRE
 builder.Services
-    .AddDbContext<EntitiesDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PRE_EntitiesConnection")));
+	.AddDbContext<EntitiesDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PRE_EntitiesConnection")));
 #endif
 #if PROD
 builder.Services
-    .AddDbContext<EntitiesDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PROD_EntitiesConnection")));
+	.AddDbContext<EntitiesDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PROD_EntitiesConnection")));
 #endif
 // Identity
 builder.Services
-    .AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
-    .AddRoles<Role>()
-    .AddEntityFrameworkStores<EntitiesDbContext>()
-    .AddDefaultUI();
+	.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = false)
+	.AddRoles<Role>()
+	.AddEntityFrameworkStores<EntitiesDbContext>()
+	.AddDefaultUI();
 
 // Services
 builder.Services
-    .AddScoped(typeof(BusinessLogic<>))
-    .AddScoped(typeof(UserBL));
+	.AddScoped(typeof(BusinessLogic<>))
+	.AddScoped(typeof(UserBL));
 
 builder.Services
-    .AddMvc(i => i.Conventions.Add(new GenericControllerRouteConvention()))
-    .ConfigureApplicationPartManager(m => m.FeatureProviders.Add(new GenericTypeControllerFeatureProvider()));
+	.AddMvc(i => i.Conventions.Add(new GenericControllerRouteConvention()))
+	.ConfigureApplicationPartManager(m => m.FeatureProviders.Add(new GenericTypeControllerFeatureProvider()));
 
-// Adding Authentication
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-//// Adding Jwt Bearer
-.AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = builder.Configuration["JWT:ValidAudience"],
-        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
-    };
-});
+// Adding Authentication with Jwt Bearer
+builder.Services
+	.AddAuthentication(options =>
+	{
+		options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+		options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+	})
+	.AddJwtBearer(options =>
+	{
+		options.SaveToken = true;
+		options.RequireHttpsMetadata = false;
+		options.TokenValidationParameters = new TokenValidationParameters()
+		{
+			ValidateIssuer = true,
+			ValidateAudience = true,
+			ValidAudience = builder.Configuration["JWT:ValidAudience"],
+			ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+			IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+		};
+	});
 
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Password settings.
-    options.Password.RequireDigit = true;
-    options.Password.RequireLowercase = true;
-    options.Password.RequireNonAlphanumeric = true;
-    options.Password.RequireUppercase = true;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
+// Adding Authorization
+builder.Services
+	.AddAuthorization();
 
-    // Lockout settings.
-    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
-    options.Lockout.MaxFailedAccessAttempts = 5;
-    options.Lockout.AllowedForNewUsers = true;
+// Logging settings
+builder.Services
+	.Configure<IdentityOptions>(options =>
+	{
+		// Password settings.
+		options.Password.RequireDigit = true;
+		options.Password.RequireLowercase = true;
+		options.Password.RequireNonAlphanumeric = true;
+		options.Password.RequireUppercase = true;
+		options.Password.RequiredLength = 6;
+		options.Password.RequiredUniqueChars = 1;
 
-    // User settings.
-    options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-    options.User.RequireUniqueEmail = false;
-});
+		// Lockout settings.
+		options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+		options.Lockout.MaxFailedAccessAttempts = 5;
+		options.Lockout.AllowedForNewUsers = true;
+
+		// User settings.
+		options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+		options.User.RequireUniqueEmail = false;
+	});
 
 // Add services to the container.
 builder.Services
-    .AddControllersWithViews()
-    .AddRazorPagesOptions(options => {
-        // options.Conventions.AddAreaPageRoute("Identity", "/Account/Login", "");
-    });
+	.AddControllersWithViews()
+	.AddRazorPagesOptions(options =>
+	{
+		// options.Conventions.AddAreaPageRoute("Identity", "/Account/Login", "");
+	});
 
 builder.Services.AddEndpointsApiExplorer();
 
-builder.Services.AddSwaggerGen(swagger => {
+// Swagger settings
+builder.Services.AddSwaggerGen(swagger =>
+{
 
-    //swagger.OrderActionsBy((apiDesc) => { 
-    //    return $"{apiDesc.ActionDescriptor.RouteValues["controller"]}"; 
-    //});
+	//swagger.OrderActionsBy((apiDesc) => { 
+	//    return $"{apiDesc.ActionDescriptor.RouteValues["controller"]}"; 
+	//});
 
-    swagger.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Generic API",
-        Description = "Operaciones CRUD para manejo del sistema.",
-        TermsOfService = new Uri("https://example.com/terms"),
-        Contact = new OpenApiContact
-        {
-            Name = "Desarrollador - Marcos Bustamante Mateo",
-            Email = "marcosbustamantemateo@gmail.com",
-            Url = new Uri("http://www.marcosbustamanteomateo.com")
-        },
-        License = new OpenApiLicense
-        {
-            Name = "License",
-            Url = new Uri("https://example.com/license")
-        },
-    });
+	swagger.SwaggerDoc("v1", new OpenApiInfo
+	{
+		Version = "v1",
+		Title = "Generic API",
+		Description = "Operaciones CRUD para manejo del sistema.",
+		TermsOfService = new Uri("https://example.com/terms"),
+		Contact = new OpenApiContact
+		{
+			Name = "Desarrollador - Marcos Bustamante Mateo",
+			Email = "marcosbustamantemateo@gmail.com",
+			Url = new Uri("http://www.marcosbustamanteomateo.com")
+		},
+		License = new OpenApiLicense
+		{
+			Name = "License",
+			Url = new Uri("https://example.com/license")
+		},
+	});
 
-    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header"
-    });
+	swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		Type = SecuritySchemeType.ApiKey,
+		Scheme = "Bearer",
+		BearerFormat = "JWT",
+		In = ParameterLocation.Header,
+		Description = "JWT Authorization header"
+	});
 
-    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] {}
+	swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[] {}
 
-        }
-    });
+		}
+	});
 
-    // Set the comments path for the Swagger JSON and UI.
-    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    swagger.IncludeXmlComments(xmlPath);
+	// Set the comments path for the Swagger JSON and UI.
+	var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+	var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+	swagger.IncludeXmlComments(xmlPath);
 });
 
 var app = builder.Build();
@@ -154,9 +163,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 // Para que devuelva los códigos de error
@@ -173,7 +182,7 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
